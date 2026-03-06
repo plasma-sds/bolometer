4328c189deae5e707a6b5ea8a351201b815cfe7f
Ferenc Lengyel <leferi99@gmail.com>
2026-03-06 11:06:53 +0100

    changed for newer data

--- AUG-AXUV/AXUV/parallel_submit_raytransfer.sh
+++ AUG-AXUV/AXUV/parallel_submit_raytransfer.sh
@@ -1,6 +1,6 @@
 export datadir="/tok/u/lefer/work/AUG-AXUV/AXUV/data"
 
-export currentdir="LF_FV_Ne1"
+export currentdir="40673/P1"
 
 mkdir -p $datadir/$currentdir/stdout
 mkdir -p $datadir/$currentdir/output
@@ -11,7 +11,7 @@ wait
 
 echo "$currentdir done"
 
-export currentdir="LF_FV_Ne0.12"
+export currentdir="41007/P1"
 
 mkdir -p $datadir/$currentdir/stdout
 mkdir -p $datadir/$currentdir/output
@@ -22,46 +22,3 @@ wait
 
 echo "$currentdir done"
 
-export currentdir="LF_FV_Ne10"
-
-mkdir -p $datadir/$currentdir/stdout
-mkdir -p $datadir/$currentdir/output
-
-ls $datadir/$currentdir/input | parallel -j 10 "nice -n 5 taskset -c {#} python3 -u calculate_emissions_for_raytransfer.py '$datadir/$currentdir/input/{}' &> $datadir/$currentdir/stdout/output_{}.txt" 
-
-wait
-
-echo "$currentdir done"
-
-export currentdir="LF_HV_Ne10"
-
-mkdir -p $datadir/$currentdir/stdout
-mkdir -p $datadir/$currentdir/output
-
-ls $datadir/$currentdir/input | parallel -j 10 "nice -n 5 taskset -c {#} python3 -u calculate_emissions_for_raytransfer.py '$datadir/$currentdir/input/{}' &> $datadir/$currentdir/stdout/output_{}.txt" 
-
-wait
-
-echo "$currentdir done"
-
-export currentdir="SF_FV_Ne0.12"
-
-mkdir -p $datadir/$currentdir/stdout
-mkdir -p $datadir/$currentdir/output
-
-ls $datadir/$currentdir/input | parallel -j 10 "nice -n 5 taskset -c {#} python3 -u calculate_emissions_for_raytransfer.py '$datadir/$currentdir/input/{}' &> $datadir/$currentdir/stdout/output_{}.txt" 
-
-wait
-
-echo "$currentdir done"
-
-export currentdir="SF_FV_Ne10"
-
-mkdir -p $datadir/$currentdir/stdout
-mkdir -p $datadir/$currentdir/output
-
-ls $datadir/$currentdir/input | parallel -j 10 "nice -n 5 taskset -c {#} python3 -u calculate_emissions_for_raytransfer.py '$datadir/$currentdir/input/{}' &> $datadir/$currentdir/stdout/output_{}.txt" 
-
-wait
-
-echo "$currentdir done"
