342b55b71f8e1822f400cadfed240c366527a152
Ferenc Lengyel <dev@lengyelf.eu>
2026-05-07 17:45:09 +0200

    Refactor 1D observation code

--- AUG-AXUV/sightline_DREAMoutput_Ne_SPI.py
+++ AUG-AXUV/sightline_DREAMoutput_Ne_SPI.py
@@ -1,18 +1,15 @@
-import os
-import sys
+import argparse
+import bisect
 import csv
 import pickle
 import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 import h5py
-import bisect
 import numpy as np
 from scipy.interpolate import CubicSpline
 
 from scipy.constants import electron_mass, atomic_mass
 
-from raysect.core import Point3D, Vector3D, translate, rotate, Point2D, rotate_basis
+from raysect.core import Vector3D, translate, rotate_basis
 from raysect.optical import World
 from raysect.primitive import Cylinder
 from raysect.optical.observer import SightLine, SpectralPowerPipeline0D, SpectralRadiancePipeline0D
@@ -22,217 +19,192 @@ from cherab.core import Species, Maxwellian, Plasma, Line
 from cherab.core.atomic import deuterium, neon
 from cherab.core.model import ExcitationLine, GaussianLine, RecombinationLine, Bremsstrahlung
 from cherab.openadas import OpenADAS
-from cherab.openadas.repository import populate
 
 from axuv.io import DATADIR, PROJECT_ROOT
 
 
-# change matplotlib renderer so that when calling observe() on observers the script does not get interrupted
+# must be set before any pyplot import; keeps observe() from blocking on headless machines
 matplotlib.use('Agg')
 
-# Download the Ne data from OPENADAS if create.py has been modified correctly
-# populate()
-
-# Convenient constants
 XAXIS = Vector3D(1, 0, 0)
-YAXIS = Vector3D(0, 1, 0)
 ZAXIS = Vector3D(0, 0, 1)
-ORIGIN = Point3D(0, 0, 0)
 
-# read time dependent densities from DREAM output file
-with h5py.File(DATADIR + "dream_output.h5") as do:
-    do_ions = do["eqsys/n_i"][()]
-    do_n_cold = do["eqsys/n_cold"][()]
-    do_n_hot = do["eqsys/n_hot"][()]
-    do_n_re = do["eqsys/n_re"][()]
-    do_n_free = do_n_cold + do_n_hot + do_n_re
-    do_time = 2.3 + do["grid/t"][()]
-    do_minor_radius = do["grid/r"][()]
-    do_minor_radius_edges = do["grid/r_f"][()]
-    do_temperature = do["eqsys/T_cold"][()]
-    do_radiated_power = do["other/fluid/Tcold_radiation"][()]
-    d0_density = do_ions[:, 0, :]
-    d0i_density = do_ions[:, 2, :]
-    d1_density = do_ions[:, 1, :]
-    d1i_density = do_ions[:, 3, :]
-    ne_densities = do_ions[:, 4:, :]
-
-# pass the time from the terminal when to observe the plasma
-ti = bisect.bisect_left(do_time, float(sys.argv[1]))
-
-# The plasma will be a cylindrical plasma
 MAJOR_RADIUS = 1.7365249  # from DREAM
-MINOR_RADIUS = do_minor_radius_edges[-1]
 CENTRE_Z = 0
 CYLINDER_HEIGHT = 2
 
 
 class CustomFunction:
-    """3D function with data interpolation for temperature and densities"""
+    """3D radial interpolator for temperature and density profiles."""
+
     def __init__(self, radialgrid, data):
-        self.data = data
         self.radialgrid = radialgrid
-        self.dr = abs(self.radialgrid[-1] - self.radialgrid[-2])
-        self.interpolated = CubicSpline(self.radialgrid, self.data)
+        self.dr = abs(radialgrid[-1] - radialgrid[-2])
+        self.interpolated = CubicSpline(radialgrid, data)
 
     def __call__(self, x, y, z):
-        if abs(z) < (CYLINDER_HEIGHT / 2):
-            xnew = x - MAJOR_RADIUS
-            ynew = y
-            radius = np.sqrt(xnew ** 2 + ynew ** 2)
-            if radius <= (self.radialgrid[-1] + (self.dr / 2)):
-                return self.interpolated(radius)
-            else:
-                return 0.
-        else:
+        if abs(z) >= CYLINDER_HEIGHT / 2:
             return 0.
+        radius = np.sqrt((x - MAJOR_RADIUS) ** 2 + y ** 2)
+        if radius <= self.radialgrid[-1] + self.dr / 2:
+            return self.interpolated(radius)
+        return 0.
 
-world = World()
 
-# SightLine geometry with transform: will look in the direction of XAXIS, with "up" being the ZAXIS
-los = SightLine(parent=world, transform=rotate_basis(XAXIS, ZAXIS)*translate(0, 0, 0))
-sppipeline = SpectralPowerPipeline0D()  # default for sightline
-sradpipeline = SpectralRadiancePipeline0D()
-los.pipelines = [sppipeline, sradpipeline]  # this can be later accessed
-los.spectral_rays = 1
-los.spectral_bins = 2500
-los.ray_extinction_prob = 0.01
-los.min_wavelength = 0.25
-los.max_wavelength = 1240
-los.pixel_samples = 3e4
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        description="Observe plasma spectrum from DREAM output along a sightline.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("time", type=float, help="Observation time [s].")
+    parser.add_argument(
+        "--pixel-samples", type=float, default=3e4, metavar="N",
+        help="Number of rays per pixel (los.pixel_samples).",
+    )
+    parser.add_argument(
+        "--pickle", action="store_true",
+        help="Also export the spectral radiance object as a pickle file.",
+    )
+    parser.add_argument(
+        "--download-adas", action="store_true",
+        help="Run populate() from cherab.openadas.repository before simulation.",
+    )
+    return parser.parse_args()
 
-###################################################
-# Produce a radiating, cylindrical plasma with Neon
-###################################################
-print("Producing the cylindrical plasma...")
-# atomic data source
-adas = OpenADAS(permit_extrapolation=True)
 
-# PLASMA ##########################################
-sigma = 0.25
-plasma = Plasma(parent=world)
-plasma.atomic_data = adas
-plasma.geometry = Cylinder(radius=MAJOR_RADIUS + MINOR_RADIUS, height=CYLINDER_HEIGHT,
-                           transform=translate(0, 0, CENTRE_Z - CYLINDER_HEIGHT / 2))
-plasma.geometry_transform = None
-plasma.integrator = NumericalIntegrator(step=sigma / 5.0)
+if __name__ == "__main__":
+    args = _parse_args()
 
-# define basic distributions
-bulk_velocity = Vector3D(0, 0, 0)
+    if args.download_adas:
+        from cherab.openadas.repository import populate
+        populate()
 
-deuterium_mass = deuterium.atomic_weight * atomic_mass
-neon_mass = neon.atomic_weight * atomic_mass
+    # Load DREAM output
+    with h5py.File(DATADIR + "dream_output.h5") as do:
+        do_ions = do["eqsys/n_i"][()]
+        do_n_cold = do["eqsys/n_cold"][()]
+        do_n_hot = do["eqsys/n_hot"][()]
+        do_n_re = do["eqsys/n_re"][()]
+        do_n_free = do_n_cold + do_n_hot + do_n_re
+        do_time = 2.3 + do["grid/t"][()]
+        do_minor_radius = do["grid/r"][()]
+        do_minor_radius_edges = do["grid/r_f"][()]
+        do_temperature = do["eqsys/T_cold"][()]
+        do_radiated_power = do["other/fluid/Tcold_radiation"][()]
+        d0_density = do_ions[:, 0, :]
+        d0i_density = do_ions[:, 2, :]
+        d1_density = do_ions[:, 1, :]
+        d1i_density = do_ions[:, 3, :]
+        ne_densities = do_ions[:, 4:, :]
 
-temperature = CustomFunction(do_minor_radius, do_temperature[ti, :])
+    ti = bisect.bisect_left(do_time, args.time)
+    if ti >= len(do_time):
+        raise SystemExit(f"Time {args.time} s is beyond DREAM grid end {do_time[-1]:.4f} s")
 
-electron_density = CustomFunction(do_minor_radius, do_n_free[ti, :])
+    MINOR_RADIUS = do_minor_radius_edges[-1]
 
-fd0_density = CustomFunction(do_minor_radius, d0_density[ti, :] + d0i_density[ti, :])
-fd1_density = CustomFunction(do_minor_radius, d1_density[ti, :] + d1i_density[ti, :])
+    # Scene
+    world = World()
 
-fne0_d = CustomFunction(do_minor_radius, ne_densities[ti, 0, :])
-fne1_d = CustomFunction(do_minor_radius, ne_densities[ti, 1, :])
-fne2_d = CustomFunction(do_minor_radius, ne_densities[ti, 2, :])
-fne3_d = CustomFunction(do_minor_radius, ne_densities[ti, 3, :])
-fne4_d = CustomFunction(do_minor_radius, ne_densities[ti, 4, :])
-fne5_d = CustomFunction(do_minor_radius, ne_densities[ti, 5, :])
-fne6_d = CustomFunction(do_minor_radius, ne_densities[ti, 6, :])
-fne7_d = CustomFunction(do_minor_radius, ne_densities[ti, 7, :])
-fne8_d = CustomFunction(do_minor_radius, ne_densities[ti, 8, :])
-fne9_d = CustomFunction(do_minor_radius, ne_densities[ti, 9, :])
-fne10_d = CustomFunction(do_minor_radius, ne_densities[ti, 10, :])
+    # SightLine looks in the direction of XAXIS with ZAXIS up
+    los = SightLine(parent=world, transform=rotate_basis(XAXIS, ZAXIS) * translate(0, 0, 0))
+    sppipeline = SpectralPowerPipeline0D()
+    sradpipeline = SpectralRadiancePipeline0D()
+    los.pipelines = [sppipeline, sradpipeline]
+    los.spectral_rays = 1
+    los.spectral_bins = 2500
+    los.ray_extinction_prob = 0.01
+    los.min_wavelength = 0.25
+    los.max_wavelength = 1240
+    los.pixel_samples = args.pixel_samples
 
-e_distribution = Maxwellian(electron_density, temperature, bulk_velocity, electron_mass)
+    # Plasma
+    print("Building the cylindrical plasma...")
+    adas = OpenADAS(permit_extrapolation=True)
 
-d0_distribution = Maxwellian(fd0_density, temperature, bulk_velocity, deuterium_mass)
-d1_distribution = Maxwellian(fd1_density, temperature, bulk_velocity, deuterium_mass)
+    sigma = 0.25
+    plasma = Plasma(parent=world)
+    plasma.atomic_data = adas
+    plasma.geometry = Cylinder(
+        radius=MAJOR_RADIUS + MINOR_RADIUS, height=CYLINDER_HEIGHT,
+        transform=translate(0, 0, CENTRE_Z - CYLINDER_HEIGHT / 2),
+    )
+    plasma.geometry_transform = None
+    plasma.integrator = NumericalIntegrator(step=sigma / 5.0)
 
-ne0_distribution = Maxwellian(fne0_d, temperature, bulk_velocity, neon_mass)
-ne1_distribution = Maxwellian(fne1_d, temperature, bulk_velocity, neon_mass)
-ne2_distribution = Maxwellian(fne2_d, temperature, bulk_velocity, neon_mass)
-ne3_distribution = Maxwellian(fne3_d, temperature, bulk_velocity, neon_mass)
-ne4_distribution = Maxwellian(fne4_d, temperature, bulk_velocity, neon_mass)
-ne5_distribution = Maxwellian(fne5_d, temperature, bulk_velocity, neon_mass)
-ne6_distribution = Maxwellian(fne6_d, temperature, bulk_velocity, neon_mass)
-ne7_distribution = Maxwellian(fne7_d, temperature, bulk_velocity, neon_mass)
-ne8_distribution = Maxwellian(fne8_d, temperature, bulk_velocity, neon_mass)
-ne9_distribution = Maxwellian(fne9_d, temperature, bulk_velocity, neon_mass)
-ne10_distribution = Maxwellian(fne10_d, temperature, bulk_velocity, neon_mass)
+    bulk_velocity = Vector3D(0, 0, 0)
+    deuterium_mass = deuterium.atomic_weight * atomic_mass
+    neon_mass = neon.atomic_weight * atomic_mass
 
-d0_s = Species(deuterium, 0, d0_distribution)
-d1_s = Species(deuterium, 1, d1_distribution)
+    temperature = CustomFunction(do_minor_radius, do_temperature[ti, :])
+    electron_density = CustomFunction(do_minor_radius, do_n_free[ti, :])
 
-ne0_s = Species(neon, 0, ne0_distribution)
-ne1_s = Species(neon, 1, ne1_distribution)
-ne2_s = Species(neon, 2, ne2_distribution)
-ne3_s = Species(neon, 3, ne3_distribution)
-ne4_s = Species(neon, 4, ne4_distribution)
-ne5_s = Species(neon, 5, ne5_distribution)
-ne6_s = Species(neon, 6, ne6_distribution)
-ne7_s = Species(neon, 7, ne7_distribution)
-ne8_s = Species(neon, 8, ne8_distribution)
-ne9_s = Species(neon, 9, ne9_distribution)
-ne10_s = Species(neon, 10, ne10_distribution)
+    fd0_density = CustomFunction(do_minor_radius, d0_density[ti, :] + d0i_density[ti, :])
+    fd1_density = CustomFunction(do_minor_radius, d1_density[ti, :] + d1i_density[ti, :])
 
-# define species
-plasma.b_field = Vector3D(1.0, 1.0, 1.0)  # not sure about this
-plasma.electron_distribution = e_distribution  # this is the free electron distribution
-plasma.composition = [d0_s, d1_s, ne0_s, ne1_s, ne2_s, ne3_s, ne4_s, ne5_s, ne6_s, ne7_s, ne8_s, ne9_s, ne10_s]
+    e_distribution = Maxwellian(electron_density, temperature, bulk_velocity, electron_mass)
+    d0_distribution = Maxwellian(fd0_density, temperature, bulk_velocity, deuterium_mass)
+    d1_distribution = Maxwellian(fd1_density, temperature, bulk_velocity, deuterium_mass)
 
-# setup two Balmer lines, need more? probably not, but could add more later
-hydrogen_I_410 = Line(deuterium, 0, (6, 2))
-hydrogen_I_396 = Line(deuterium, 0, (7, 2))
+    ne_density_funcs = [
+        CustomFunction(do_minor_radius, ne_densities[ti, i, :]) for i in range(11)
+    ]
+    ne_distributions = [
+        Maxwellian(f, temperature, bulk_velocity, neon_mass) for f in ne_density_funcs
+    ]
+    ne_species = [Species(neon, i, ne_distributions[i]) for i in range(11)]
 
-########################################################################
-# Get Neon lines from Photon Emissivity Coefficients datafiles
-# This "ne" folder would normally be located at ~/.cherab/openadas/repository/pec/exctitaion/ne
-# or at .../pec/recombination/ne The JSON files are the same in both directories
-########################################################################
-neon_lines = []
-with open(DATADIR + "ne.csv") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        i, part1, part2 = row
+    plasma.b_field = Vector3D(1.0, 1.0, 1.0)
+    plasma.electron_distribution = e_distribution
+    plasma.composition = [
+        Species(deuterium, 0, d0_distribution),
+        Species(deuterium, 1, d1_distribution),
+        *ne_species,
+    ]
 
-        neon_lines.append(
-            ExcitationLine(
-                Line(neon, int(i), (part1, part2)), lineshape=GaussianLine
-            )
-        )
-        neon_lines.append(
-            RecombinationLine(
-                Line(neon, int(i), (part1, part2)), lineshape=GaussianLine
-            )
-        )
+    # Deuterium lines: full Lyman (n→1, 91–122 nm) and Balmer (n→2, 375–656 nm) series
+    deuterium_lines = []
+    for lower_n, upper_range in [(1, range(2, 13)), (2, range(3, 13))]:
+        for upper_n in upper_range:
+            line = Line(deuterium, 0, (upper_n, lower_n))
+            deuterium_lines.append(ExcitationLine(line, lineshape=GaussianLine))
+            deuterium_lines.append(RecombinationLine(line, lineshape=GaussianLine))
 
-# add all lines to the plasma
-plasma.models = [
-    ExcitationLine(hydrogen_I_410, lineshape=GaussianLine),
-    RecombinationLine(hydrogen_I_410, lineshape=GaussianLine),
-    ExcitationLine(hydrogen_I_396, lineshape=GaussianLine),
-    RecombinationLine(hydrogen_I_396, lineshape=GaussianLine),
-    *neon_lines,
-    Bremsstrahlung()
-]
+    # Neon lines from ne.csv
+    neon_lines = []
+    with open(DATADIR + "ne.csv") as f:
+        for i, part1, part2 in csv.reader(f):
+            line = Line(neon, int(i), (part1, part2))
+            neon_lines.append(ExcitationLine(line, lineshape=GaussianLine))
+            neon_lines.append(RecombinationLine(line, lineshape=GaussianLine))
 
-########################################################################
-# Observe the radiation spectrum along the LOS
-########################################################################
-print("Observing the radiation spectrum along the LOS...")
-los.observe()
+    plasma.models = [*deuterium_lines, *neon_lines, Bremsstrahlung()]
 
-photon_energies = 1239.8 / sppipeline.wavelengths
-datatosave = np.array([sppipeline.samples.mean,
-                       sppipeline.samples.variance,
-                       sppipeline.wavelengths,
-                       photon_energies])
-folder_name = PROJECT_ROOT / "output" / "0D_data"
-if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
-np.save(folder_name / "highres_{:.4f}s.npy".format(do_time[ti]), datatosave)
+    # Observe
+    print("Observing the radiation spectrum along the LOS...")
+    los.observe()
 
-# # Optionally dump the spectrum objects with pickle
-# spectrum = sradpipeline.to_spectrum()
-# filehandler = open("saved_data_new/highres_{:.4f}s_spectrum.obj".format(do_time[ti]), 'wb')
-# pickle.dump(spectrum, filehandler)
-print("Saved array and dumped object")
+    # Save HDF5
+    folder_name = PROJECT_ROOT / "output" / "1D_data"
+    folder_name.mkdir(parents=True, exist_ok=True)
+    hdf5_path = folder_name / "highres_{:.4f}s.h5".format(do_time[ti])
+
+    with h5py.File(hdf5_path, "w") as h5f:
+        h5f.create_dataset("spectral_power_mean",     data=sppipeline.samples.mean)
+        h5f.create_dataset("spectral_power_variance", data=sppipeline.samples.variance)
+        h5f.create_dataset("wavelengths_nm",          data=sppipeline.wavelengths)
+        h5f.create_dataset("photon_energies_eV",      data=1239.8 / sppipeline.wavelengths)
+        h5f.attrs["observation_time_s"] = float(do_time[ti])
+        h5f.attrs["pixel_samples"]      = args.pixel_samples
+        h5f.attrs["spectral_bins"]      = los.spectral_bins
+        h5f.attrs["min_wavelength_nm"]  = los.min_wavelength
+        h5f.attrs["max_wavelength_nm"]  = los.max_wavelength
+
+    print(f"Saved HDF5 to {hdf5_path}")
+
+    if args.pickle:
+        spectrum = sradpipeline.to_spectrum()
+        pickle_path = folder_name / "highres_{:.4f}s_spectrum.pkl".format(do_time[ti])
+        with open(pickle_path, "wb") as fh:
+            pickle.dump(spectrum, fh)
+        print(f"Saved spectrum pickle to {pickle_path}")
